2e5f38b7ca7decd7af0db06dcf6db3ca3b07b6b3
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2024-12-11 20:10:13 +0300

    TokenAuthorize

---
+++ WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Test()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+
+        [Authorize]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("HoşGeldiniz");
+        }
+    }
+}
---
+++ WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+    public class CreateToken
+    {
+        public string TokenCreate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);//default password algorithm
+
+            JwtSecurityToken token = new JwtSecurityToken
+                (issuer: "http://localhost"
+                , audience: "http://localhost"
+                , notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(3)
+                , signingCredentials: credentials);//necessessery parameters for create token
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+
+
+        }
+    }
+}
