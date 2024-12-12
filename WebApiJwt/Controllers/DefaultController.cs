3291351315f83aaba78a0a8b0d28c05fa36a2fc1
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2024-12-12 17:21:57 +0300

    85

--- WebApiJwt/Controllers/DefaultController.cs
+++ WebApiJwt/Controllers/DefaultController.cs
@@ -9,19 +9,31 @@ namespace WebApiJwt.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
+
         [HttpGet("[action]")]
-        public IActionResult Test()
+        public IActionResult TokenOlustur()
         {
             return Ok(new CreateToken().TokenCreate());
         }
 
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
 
         [Authorize]
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [HttpGet("[action]")]
         public IActionResult Test2()
         {
-            return Ok("HoşGeldiniz");
+            return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
         }
     }
 }
