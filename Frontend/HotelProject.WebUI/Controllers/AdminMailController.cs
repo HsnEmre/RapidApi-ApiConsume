ab35faec762435e178c3785a347fdf6532d41942
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2025-03-03 21:37:54 +0300

    admincontact mail işlemleri

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,48 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(AdminViewModel model)
+        {
+
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxFromAddress = new MailboxAddress("HotelierAdmin", "Burası Bizim Mailimiz");
+            mimeMessage.From.Add(mailboxFromAddress);
+
+
+            MailboxAddress mailboxAdressTo = new MailboxAddress("User", model.ReceiverMail);
+
+
+            mimeMessage.To.Add(mailboxAdressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("mail", "passwordKey");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+    }
+}
