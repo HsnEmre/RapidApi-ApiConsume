bf729f02c198ea292a99d2e78edc23c580c93f88
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2025-03-06 14:22:42 +0300

    hata

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,14 @@
 ﻿using HotelProject.WebUI.Dtos.BookingDto;
 using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.MessageCategoryDto;
 using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +25,29 @@ namespace HotelProject.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+
+
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("http://localhost:35160/api/MessageCategory");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsondata = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultroomDto>>(jsondata);
-                return View(values);
-            }
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDto>>(jsonData);
+
+
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text=x.MessageCategoryName,
+                                                Value=x.MessageCategoryID.ToString()
+                                            }).ToList();
+
+
+            ViewBag.v=values2;
 
             return View();
+
         }
 
         [HttpGet]
